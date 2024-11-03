89de5091f1659c7e07e95e38df6c1ca753443ce6
Haferm98 <105403350+Haferm98@users.noreply.github.com>
2024-11-03 14:42:57 -0500

    Fetch product data and display basic information.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// Task 2: Fetch Products from the API using Fetch and Promises
 
+const productList = document.getElementById('productList');
+    fetch('https://www.course-api.com/javascript-store-products');
+        .then(response => 
+        {
+            if (!response.ok)
+            {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        }
+        )
